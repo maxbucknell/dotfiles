c7e0473bbf2cfb2a045c0c43d5d06a26a4cff481
Max Bucknell <me@maxbucknell.com>
2018-01-22 09:56:18 +0000

    Add Magento 2 init scripts

--- lib/python/mbutils.py
+++ lib/python/mbutils.py
@@ -7,17 +7,22 @@ from functools import cmp_to_key
 
 def find_in_parent(filename, starting_dir=None):
     '''Look up the directory tree until you find a file.'''
+    root = find_root(filename, starting_dir)
+    return path.join(root, filename)
+
+def find_root(filename, starting_dir=None):
+    '''Look up the directory tree until you find a file.'''
     if starting_dir is None:
         starting_dir = os.getcwd()
 
     candidate_file = path.join(starting_dir, filename)
 
     if (path.isfile(candidate_file)):
-        return candidate_file
+        return starting_dir
     elif starting_dir == '/':
         raise IOError('File not found in any directory')
     else:
-        return find_in_parent(filename, path.dirname(starting_dir))
+        return find_root(filename, path.dirname(starting_dir))
 
 def _parse_composer_manifest(path):
     '''Parse composer.json into dict'''
